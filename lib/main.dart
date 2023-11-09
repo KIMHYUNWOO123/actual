396a8638b34d7ee98171a02bdae6b6dd843c714f
김현우 <wry1004@cocen.com>
2023-11-09 09:25:37 +0900

    23/11/09

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:actual/user/view/login_screen.dart';
+import 'package:actual/common/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +13,7 @@ class _App extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'NotoSans'),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
